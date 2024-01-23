dcc302a9bb39606e6168078ed00875860b12721c
Rohit242003 <155900331+Rohit242003@users.noreply.github.com>
2024-01-23 15:38:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express= require("express");
+const cors= require("cors");
+const nodemailer= require("nodemailer");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.post("/se",(req,res)=>{
+		let name = req.body.name;
+		let txt = "phone" + req.body.phone +"query="+req.body.query;
+
+		let transporter=nodemailer.createTransport({
+			service:"gmail",
+			auth:{
+				user:"mayur.react.aug@gmail.com",
+				pass:"avsfycxcotqquwsw"
+				}
+		})
+	let mailOptions={
+			from:"mayur.react.aug@gmail.com",
+			to:"rohit.borse.comp@gmail.com",
+			subject:"enquiry from"+name,
+			text:txt
+			}
+	
+			transporter.sendMail(mailOptions,(err,info)=>{
+					if (err)
+						res.status(500).json({success:true,message:"server error"});
+					else 
+						res.status(200).json({success:true,message:"email send"});
+				})
+
+			});
+app.listen(9000,()=>{console.log("ready to serve @9000");});
+	
